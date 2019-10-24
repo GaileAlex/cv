367f4a35d0db72e5ee22bd4a8c18e47d5ab97a58
Aleksei Gaile <aleksei.gaile@wisercat.eu>
2019-10-24 13:06:10 +0300

    js css start footer header create

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {StartPageComponent} from "./components/start-page/start-page.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: StartPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
