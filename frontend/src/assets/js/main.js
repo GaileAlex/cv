d39ddebb4862c296119d86a00ebd0b09a3ba61fc
GaileAlex <aleksei.gaile@gmail.com>
2020-03-05 12:21:32 +0200

    refactoring front

--- frontend/src/assets/js/main.js
+++ frontend/src/assets/js/main.js
@@ -9,7 +9,7 @@ function main() {
         target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
         if (target.length) {
           $('html,body').animate({
-            scrollTop: target.offset().top
+            scrollTop: target.offset().top-50
           }, 900);
           return false;
         }
@@ -21,39 +21,6 @@ function main() {
       offset: {
         top: $('header').height()
       }
-    });
-
-    // Portfolio isotope filter
-    $(window).load(function() {
-      var $container = $('.portfolio-items');
-      $container.isotope({
-        filter: '*',
-        animationOptions: {
-          duration: 750,
-          easing: 'linear',
-          queue: false
-        }
-      });
-      $('.cat a').click(function() {
-        $('.cat .active').removeClass('active');
-        $(this).addClass('active');
-        var selector = $(this).attr('data-filter');
-        $container.isotope({
-          filter: selector,
-          animationOptions: {
-            duration: 750,
-            easing: 'linear',
-            queue: false
-          }
-        });
-        return false;
-      });
-
-    });
-
-    // Pretty Photo
-    $("a[rel^='prettyPhoto']").prettyPhoto({
-      social_tools: false
     });
 
   }());
