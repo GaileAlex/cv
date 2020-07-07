a2cb32b5597a87c6b07a312604e9f1a1eb44d46a
GaileAlex <aleksei.gaile@wisercat.eu>
2020-07-07 08:19:46 +0300

    add data

--- persistence/src/main/resources/db/changelog/v0004_insert_default_data_mindly.sql
+++ persistence/src/main/resources/db/changelog/v0004_insert_default_data_mindly.sql
@@ -1,6 +1,8 @@
 --liquibase formatted sql
 --changeset gaile:v0004_insert_default_data_mindly
 
-INSERT INTO mindly (mindly_crypto_currency, mindly_amount, mindly_date_of_purchase, mindly_wallet_location) values ( 'Bitcoin', 33, '2020-01-05', '100');
+INSERT INTO mindly (mindly_crypto_currency, mindly_amount, mindly_date_of_purchase, mindly_wallet_location) values ( 'Bitcoin', 1, '2020-01-05', 'My personal wallet');
+INSERT INTO mindly (mindly_crypto_currency, mindly_amount, mindly_date_of_purchase, mindly_wallet_location) values ( 'Ethereum', 1, '2020-01-05', 'My personal wallet');
+INSERT INTO mindly (mindly_crypto_currency, mindly_amount, mindly_date_of_purchase, mindly_wallet_location) values ( 'Ripple', 1, '2020-01-05', 'My personal wallet');
 
 
