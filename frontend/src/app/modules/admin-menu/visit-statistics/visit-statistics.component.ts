09d0e7b4a395ed4e585b42357f5d38f08b7ad8d1
GaileAlex <aleksei.gaile@wisercat.eu>
2020-08-10 22:23:57 +0300

    fix

--- frontend/src/app/modules/admin-menu/visit-statistics/visit-statistics.component.ts
+++ frontend/src/app/modules/admin-menu/visit-statistics/visit-statistics.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ChartDataSets, ChartOptions } from "chart.js";
-import { Color, Label } from "ng2-charts";
+import { ChartDataSets, ChartOptions } from 'chart.js';
+import { Color, Label } from 'ng2-charts';
 
 @Component({
   selector: 'app-visit-statistics',
@@ -11,9 +11,6 @@ export class VisitStatisticsComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
-  }
-
   chartOptions = {
     responsive: true
   };
@@ -23,5 +20,8 @@ export class VisitStatisticsComponent implements OnInit {
     { data: [45, 67, 800, 500], label: 'Account C' }
   ];
   chartLabels = ['January', 'February', 'Mars', 'April'];
+
+  ngOnInit() {
+  }
 
 }
