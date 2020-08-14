48bc967bbb38d05ee66441be761c38417e0e1b95
GaileAlex <aleksei.gaile@wisercat.eu>
2020-08-14 08:21:55 +0300

    delete unused

--- web-api/src/main/java/ee/gaile/controller/mindly/MindlyController.java
+++ web-api/src/main/java/ee/gaile/controller/mindly/MindlyController.java
@@ -18,31 +18,17 @@ import static ee.gaile.service.security.SecurityConfig.API_V1_PREFIX;
 public class MindlyController {
     private final MindlyService mindlyService;
 
-    /**
-     * getting data from the database, calculating the value of the currency
-     *
-     * @return List<Mindly>
-     */
     @GetMapping(produces = "application/json")
     @ResponseStatus(HttpStatus.OK)
     public List<Mindly> getPortfolio() {
         return mindlyService.getAllPortfolio();
     }
 
-    /**
-     * saving object to database
-     *
-     * @param portfolio get object
-     * @return save to DB
-     */
     @PostMapping(consumes = "application/json")
     public Mindly addPortfolioItem(@RequestBody Mindly portfolio) {
         return mindlyService.savePortfolio(portfolio);
     }
 
-    /**
-     * @param portfolioId id portfolio item
-     */
     @DeleteMapping("/{portfolioId}")
     @ResponseStatus(HttpStatus.OK)
     public void deletePortfolioItem(@PathVariable("portfolioId") Long portfolioId) {
