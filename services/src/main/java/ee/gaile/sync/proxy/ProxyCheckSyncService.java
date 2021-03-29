faa3e7345da389f3f3b22945fccff21e6f0aa728
GaileAlex <aleksei.gaile@gmail.com>
2021-03-29 23:39:13 +0300

    minor fix

--- services/src/main/java/ee/gaile/sync/proxy/ProxyCheckSyncService.java
+++ services/src/main/java/ee/gaile/sync/proxy/ProxyCheckSyncService.java
@@ -14,6 +14,7 @@ import java.net.Proxy;
 import java.net.URL;
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.HashMap;
 
 /**
  * Service for checking proxy for availability
@@ -102,7 +103,7 @@ public class ProxyCheckSyncService {
         double numberChecksValue = 100.0 * ((double) numberUnansweredChecks / (double) numberChecks);
 
         if (Double.isInfinite(numberChecksValue)) {
-            log.info("speed is infinite");
+            log.info("proxy speed is infinite");
             numberChecksValue = 0.0;
         }
 
