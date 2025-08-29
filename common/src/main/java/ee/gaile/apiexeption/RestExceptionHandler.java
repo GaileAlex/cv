fc5a403f970350a14bd11cb189f6933e4be47cd5
GaileAlex <aleksei.gaile@gmail.com>
2025-08-29 10:26:24 +0300

    fixed Broken pipe

--- common/src/main/java/ee/gaile/apiexeption/RestExceptionHandler.java
+++ common/src/main/java/ee/gaile/apiexeption/RestExceptionHandler.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.async.AsyncRequestNotUsableException;
 import org.springframework.web.server.ResponseStatusException;
 
 @ControllerAdvice
@@ -48,6 +49,11 @@ public class RestExceptionHandler {
 
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(objectNode);
+    }
+
+    @ExceptionHandler(AsyncRequestNotUsableException.class)
+    public void handleBrokenPipe(Exception ex) {
+        LOG.error("the following error occurred: Broken pipe");
     }
 
 }
